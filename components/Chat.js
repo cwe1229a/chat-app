454624ee97a12579d0b4a9ffb81395136c2efbef
Courtney Webster <courtney@atlmd.net>
2022-08-31 21:32:14 -0400

    updating chat.js with GiftedChat

--- components/Chat.js
+++ components/Chat.js
@@ -1,27 +1,49 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+// import { View, Text, StyleSheet } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
-export default function Chat(props) {
-  let { name, color } = props.route.params;
-  props.navigation.setOptions({ title: name });
-
-  return (
-    <View style={[{ backgroundColor: color }, styles.container]}>
-      <Text style={styles.text}>Hello Chat!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-
-  text: {
-    color: "#ffffff",
-    fontSize: 45,
-    fontWeight: "600"
+export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: []
+    };
   }
-});
+
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any"
+          }
+        }
+      ]
+    });
+  }
+
+  onSend(messages = []) {
+    let name = this.props.route.params.name;
+    this.props.navigation.setOptions({ title: name });
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages)
+    }));
+  }
+
+  render() {
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={(messages) => this.onSend(messages)}
+        user={{
+          _id: 1
+        }}
+      />
+    );
+  }
+}
